2df7a119cd79dfadadadbaf3898cdcd0b7b9b957
Vishwas <vishwasnarayan@gmail.com>
2014-08-21 16:59:05 +0530

    Initialization

---
+++ fibonacciTest1.py
@@ -0,0 +1,12 @@
+    def getListOfNumbersFromConsole():
+
+        """ Not implemented yet"""
+
+        pass
+    def computeFibonacciForNumberList(numberList):
+
+        """ Not implemented yet""" 
+
+        pass
+    if __name__=="__main__":
+        print computeFibonacciForNumberList(getListOfNumbersFromConsole())
