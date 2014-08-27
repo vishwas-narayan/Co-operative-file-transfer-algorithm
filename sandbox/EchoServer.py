f6a8543db7b5ea792d52cd73484fbf3f94575f39
Abhishek Alfred Singh <alfred.abhishek@gmail.com>
2014-08-27 22:39:27 +0530

    Adding an echo server to use with twisted matrix

---
+++ sandbox/EchoServer.py
@@ -0,0 +1,26 @@
+
+"""Requires Twisted matrix to run. To install do:  
+        sudo apt-get install python-twisted
+   ~~~~~~~~~~
+
+   To see how it works run the program as:  python EchoServer.py
+   Then open another terminal and use telnet program to connect: 
+   telnet localhost 8000
+
+   Type whatever you want then press Enter. 
+   To close the telnet connection press CTRL+] and then CTRL+D
+"""
+
+from twisted.internet import protocol, reactor
+
+
+class Echo(protocol.Protocol):
+    def dataReceived(self, data):
+        self.transport.write(data)
+
+class EchoFactory(protocol.Factory):
+    def buildProtocol(self, addr):
+        return Echo()
+
+reactor.listenTCP(8000, EchoFactory())
+reactor.run()
