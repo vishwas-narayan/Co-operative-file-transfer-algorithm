13d0b2ed14e008b6234c1e90d4257e16cde1b0e0
Abhishek Alfred Singh <alfred.abhishek@gmail.com>
2014-08-29 18:14:31 +0530

    Modified the program to perform lesser recursive calls by using a dictionary lookup. Also changed the function to a class

--- sandbox/fibs.py
+++ sandbox/fibs.py
@@ -1,14 +1,26 @@
-def _nfib(n):
-    return n if n<2 else _nfib(n-1)+_nfib(n-2)
+class FibonacciConstructor():
+    """
+    FibonacciConstructor constructs and maintains a dictionary of fibonacci numbers
+    """
+    def __init__(self):
+        self.fibsList={0:0, 1:1}
+    def _nfib(self, n):
+        if n not in self.fibsList:
+            self.fibsList[n] = self._nfib(n-1)+self._nfib(n-2)
+        return self.fibsList[n]
 
-def getFibonacciNumbers(listOfNumbers):
-    d={}
-    for n in listOfNumbers:
-        d[n]=_nfib(n)
-    return d
+    def getFibonacciNumbers(self, listOfNumbers):
+        """
+        Takes one list of integers as input, and returns a dictionary of fibonacci numbers
+        mapped as n:Fibonacci(n)
+        """
+        d={}
+        for n in listOfNumbers:
+            d[n] = self._nfib(n)
+        return d
 
 if __name__=="__main__":
     n,nList=input("Enter N: "),[]
     for i in range(n):
         nList.append(input('Enter Number'))
-    print getFibonacciNumbers(nList)
+    print FibonacciConstructor().getFibonacciNumbers(nList)
