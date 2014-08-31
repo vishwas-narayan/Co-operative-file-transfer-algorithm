745e51006c377a235d4e957c420ccc5e036c1d76
Abhishek Alfred Singh <alfred.abhishek@gmail.com>
2014-08-31 23:20:24 +0530

    modifed configs and echo server

--- sandbox/EchoServer.py
+++ sandbox/EchoServer.py
@@ -15,15 +15,35 @@
 """
 
 from twisted.internet import protocol, reactor
-
+from signal import SIGINT, signal
+from sys import exit
+import logging as LOG
+import LoggingConfig
 
 class Echo(protocol.Protocol):
+    connections=0
+    def connectionMade(self):
+        Echo.connections+=1
+        LOG.debug("Total connections: %d",Echo.connections)
+
     def dataReceived(self, data):
-        self.transport.write(data)
+        LOG.info("Received data from client: %s" ,data)
+        self.transport.write(data.upper())
+
+    def connectionLost(self,reason):
+        Echo.connections-=1
+        LOG.debug ("ConnectionLost, Total connections: %d " , Echo.connections)
+
+    @staticmethod
+    def sigintHandler(num,trace):
+        LOG.info("Quitting the program")
+        LOG.info("total connections %d" %(Echo.connections))
+        reactor.stop()
 
 class EchoFactory(protocol.Factory):
     def buildProtocol(self, addr):
         return Echo()
 if __name__=="__main__":
+    signal(SIGINT,Echo.sigintHandler)
     reactor.listenTCP(8000, EchoFactory())
     reactor.run()
