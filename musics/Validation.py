40f9ec81bfd175505ebbe35ba77e6f1bc275f3de
Abhishek Alfred Singh <alfred.abhishek@gmail.com>
2014-09-05 21:17:35 +0530

    Correcting Sushma's code for indentation errors and committing.

--- musics/Validation.py
+++ musics/Validation.py
@@ -1,29 +1,32 @@
-    
+
 class Validation:
-	"""
-	Author:Sushma
-	this is the class to check whether the format given to access the file is correct or not
-	
+    """
+        Author:Sushma
+        this is the class to check whether the format given to access the file is correct or not
+
     """
 
-	def checkValidation(self,inputGivenForFile):
-		newFormat=inputGivenForFile.split('/',1)
+    def checkValidation(self,inputGivenForFile):
+        newFormat=inputGivenForFile.split('/',1)
         newFormat1=newFormat[0]
-    	if(newFormat1=="GET " and newFormat[1]!=''):
-    		return 1
-    	else:
-        	return 0
+        if(newFormat1=="GET " and newFormat[1]!=''):
+            return 1
+        else:
+            return 0
+        
+if __name__=="__main__":
+    validationObject=Validation()
 
-validationObject=Validation()
+    if((validationObject.checkValidation("GET /text/page/asd.html"))==1):
+        print "GET /text/page/asd.html is correct format"
 
-if((validationObject.checkValidation("GET /text/page/asd.html"))==1):
-	print "GET /text/page/asd.html is correct format"
+    if((validationObject.checkValidation("GET \text/page/asd.html"))!=1):
+        print "GET \\text/page/asd.html is wrong format"
 
-if((validationObject.checkValidation("GET \text/page/asd.html"))!=1):
-	print "GET \\text/page/asd.html is wrong format"
+    if((validationObject.checkValidation("GET/text/page/asd.html"))!=1):
+        print "GET/text/page/asd.html is wrong format"    
 
-if((validationObject.checkValidation("GET/text/page/asd.html"))!=1):
-	print "GET/text/page/asd.html is wrong format"    
-
-if((validationObject.checkValidation("GET /"))!=1):
-	print "GET / is wrong format"
+    if((validationObject.checkValidation("GET /"))!=1):
+        print "GET / is wrong format"
+    text=raw_input("Enter text to validate: ")
+    print ("Text : %s is valid? %d" %(text,validationObject.checkValidation(text)))
