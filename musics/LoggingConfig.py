453c0d3e4eef197b0a61f9f5c1a52b33121d9207
Abhishek Alfred Singh <alfred.abhishek@gmail.com>
2014-10-09 23:25:50 +0530

    modifying logging directory

--- musics/LoggingConfig.py
+++ musics/LoggingConfig.py
@@ -1,7 +1,7 @@
 import logging as LOG
 
 LOG.basicConfig(
-    filename='musics.log',
+    filename='/tmp/musics.log',
     level=LOG.DEBUG,
     format='%(asctime)s %(levelname)s : %(module)s.%(funcName)s: %(message)s', 
     datefmt='%m/%d/%Y %I:%M:%S %p')
