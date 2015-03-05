397c562fc278a5719e2e57f962942e9c52511bfe
Vishwas <vishwasnarayan@gmail.com>
2015-03-05 08:48:35 +0530

    Adding Argparse and client Sync of ClientDownload.py

--- musics/ClientDownload.py
+++ musics/ClientDownload.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import tempfile
 from twisted.internet import reactor, protocol
@@ -5,6 +6,11 @@ import logging as LOG
 from ipadress import ip
 import LoggingConfig
 from BlockCreater import DS, BlockCreator
+
+parser = argparse.ArgumentParser()
+parser.add_argument("start",help = "File to be transferred")
+args = parser.parse_args()
+
 class EchoClient(protocol.Protocol):
     def __init__(self,echoFactory,Ide=None,filename=None):
         """5.To make the created client's id same"""
@@ -15,7 +21,7 @@ class EchoClient(protocol.Protocol):
         """6.This module is for checking whether this is first connection of the client.
         If it is not then the created instance sends message to the server about its creation"""
         if(self.id==None):
-            self.filename=raw_input("enter filename: ")
+            self.filename = args.start
             self.transport.write(BlockCreator().createInit())
         else:
             LOG.debug("Created Instance makes the server check for another instance to be created")
@@ -42,25 +48,29 @@ class EchoClient(protocol.Protocol):
             self.transport.loseConnection()
     def recieveBlock(self,data):
         self.d=data
-        f=open("newfile.txt",'a')     
-        if(self.d[DS.CONTENT_TYPE]==DS.DATA):
-            try: 
-                f.write(self.d[DS.CONTENT])
-                self.transport.write(BlockCreator(self.id).createBlockForClient(DS.GET,self.filename))        
-            except:
-                LOG.debug( "Error in converting from json")
-                self.transport.loseConnection()
-        elif(self.d[DS.CONTENT_TYPE]==DS.EOF):                          
-            try:
-                f.write(self.d[DS.CONTENT])
-                f.close()
-            except:
-                LOG.debug("")
-            LOG.debug("client loses connection because EOF is reached" )
-            self.transport.loseConnection() 
-           
+        f=open("newfile.txt",'a')
+        if(self.ef.clientSync(self.id,self.d)):     
+            if(self.d[DS.CONTENT_TYPE]==DS.DATA):
+                try: 
+                    f.write(self.d[DS.CONTENT])
+                    self.transport.write(BlockCreator(self.id).createBlockForClient(DS.GET,self.filename))        
+                except:
+                    LOG.debug( "Error in converting from json")
+                    self.transport.loseConnection()
+            elif(self.d[DS.CONTENT_TYPE]==DS.EOF):                          
+                try:
+                    f.write(self.d[DS.CONTENT])
+                    f.close()
+                except:
+                    LOG.debug("")
+                LOG.debug("client loses connection because EOF is reached" )
+                self.transport.loseConnection() 
+        else:
+           return  
 class EchoFactory(protocol.ClientFactory):
     noc=1
+    bNum={}
+    dict_store_data={}
     def __init__(self):
         self.ide=None
         self.filename=None
@@ -68,14 +78,32 @@ class EchoFactory(protocol.ClientFactory):
         return EchoClient(self,self.ide,self.filename)
         
     def getMessageFromClient(self,Id,filename):
-        """4.This method iniates the connection with the server but with the id.
+        """4.This method intiates the connection with the server but with the id.
         So that EchoFactory uses the id for creating the client"""
         LOG.debug("Message to the EchoFactory from client %d" ,Id)
         self.ide=Id
         self.filename=filename
         self.noc+=1
         reactor.connectTCP("localhost",8000,self)
-        LOG.debug("connection %d is made",self.noc)    
+        LOG.debug("connection %d is made",self.noc) 
+    def clientSync(self,cid,data):
+        flag=0
+        LOG.DEBUG("inside client sync")
+        if(self.bNum.has_key(cid)):
+            if(self.bNum[cid]==data[DS.BLOCK]):
+                self.bNum[cid]+=1
+                flag=1
+                return True
+        else:
+            self.bNum[cid]=1
+            if(data[DS.BLOCK]==self.bNum[cid]):
+                self.bNum[cid]+=1
+                flag=1
+                return True  
+        if(flag==0):
+            print "Inside flag"
+            dict_store_data[cid]=data
+        return False
 
     def clientConnectionFailed(self, connector, reason):
         print "Connection failed......"
